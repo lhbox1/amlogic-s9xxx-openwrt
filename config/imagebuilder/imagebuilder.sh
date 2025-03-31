b4810d507fe6aff9a760f949a7461342b0d5c702
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-31 13:37:43 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -195,7 +195,7 @@ rebuild_firmware() {
         \
         luci-app-samba4 luci-i18n-samba4-zh-cn luci-app-amlogic luci-i18n-amlogic-zh-cn \
         \
-        openssh-server ruby ruby-yaml firewall4 ca-bundle ip-full yq kmod-inet-diag kmod-nft-socket kmod-nft-tproxy kmod-tun \
+        openssh-sftp-server ruby ruby-yaml firewall4 ca-bundle ip-full yq kmod-inet-diag kmod-nft-socket kmod-nft-tproxy kmod-tun \
         sing-box \
         \
         ${config_list} \
